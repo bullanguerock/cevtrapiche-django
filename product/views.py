219b3aa1ed39fddbe96bef08c5df25dc7354c379
pcaynzec <toto.palacios.a@gmail.com>
2022-01-19 14:00:58 -0300

    fiz serializers

--- product/views.py
+++ product/views.py
@@ -14,8 +14,8 @@ from .serializers import ProductSerializer, CategorySerializer
 class LatestProductsList(APIView):
     def get(self, request, format=None):
         products = Product.objects.all()[0:4]
-        serializer = ProductSerializer(products,many = True, context={'request': request})
-        return Response(serializer.data)
+        serializer = ProductSerializer(products, many=True, context={'request': request})
+        return Response({"data": serializer})
 
 class ProductDetail(APIView):
     def get_object(self, category_slug, product_slug):
